50edb731bacffebcc27c5887d8de994cbf652dce
willybnt <willyuili123@gmail.com>
2024-08-15 17:49:40 -0300

    att

---
+++ workrpyc/cliente.py
@@ -0,0 +1,37 @@
+import rpyc
+
+class ClienteArquivo:
+    def __init__(self, host='localhost', port=18812):
+        self.conn = rpyc.connect(host, port)
+
+    def enviar_arquivo(self, nome_arquivo):
+        with open('C:/Users/willy/OneDrive/Documentos/faculdade/sistemasdistribuidos/workrpyc/exemplo.txt', 'rb') as f:
+            dados = f.read()
+        self.conn.root.enviar_arquivo(nome_arquivo, dados)
+
+    def listar_arquivos(self):
+        return self.conn.root.listar_arquivos()
+
+    def baixar_arquivo(self, nome_arquivo):
+        dados = self.conn.root.baixar_arquivo(nome_arquivo)
+        if dados:
+            with open(f'baixado_{nome_arquivo}', 'wb') as f:
+                f.write(dados)
+        else:
+            print("Arquivo não encontrado no servidor.")
+
+    def registrar_interesse(self, nome_arquivo, validade=60):
+        self.conn.root.registrar_interesse(nome_arquivo, self, validade)
+
+    def cancelar_interesse(self, nome_arquivo):
+        self.conn.root.cancelar_interesse(nome_arquivo, self)
+
+    def notificar_evento(self, nome_arquivo):
+        print(f"Notificação: {nome_arquivo} está agora disponível no servidor!")
+
+if __name__ == "__main__":
+    cliente = ClienteArquivo()
+    cliente.enviar_arquivo('example.txt')
+    print(cliente.listar_arquivos())
+    cliente.baixar_arquivo('example.txt')
+    cliente.registrar_interesse('arquivo_inexistente.txt')
---
+++ workrpyc/servidor.py
@@ -0,0 +1,41 @@
+import rpyc
+from rpyc.utils.server import ThreadedServer
+import os
+
+class ServidorArquivo(rpyc.Service):
+    def __init__(self):
+        self.arquivos = {}
+        self.interesses = []
+
+    def exposed_enviar_arquivo(self, nome_arquivo, dados_arquivo):
+        with open(nome_arquivo, 'wb') as f:
+            f.write(dados_arquivo)
+        self.arquivos[nome_arquivo] = nome_arquivo
+        self.verificar_interesses(nome_arquivo)
+
+    def exposed_listar_arquivos(self):
+        return list(self.arquivos.keys())
+
+    def exposed_baixar_arquivo(self, nome_arquivo):
+        if nome_arquivo in self.arquivos:
+            with open(nome_arquivo, 'rb') as f:
+                return f.read()
+        else:
+            return None
+
+    def exposed_registrar_interesse(self, nome_arquivo, cliente, validade):
+        self.interesses.append((nome_arquivo, cliente, validade))
+
+    def exposed_cancelar_interesse(self, nome_arquivo, cliente):
+        self.interesses = [(f, c, v) for f, c, v in self.interesses if not (f == nome_arquivo and c == cliente)]
+
+    def verificar_interesses(self, nome_arquivo):
+        for interesse in self.interesses:
+            if interesse[0] == nome_arquivo:
+                cliente = interesse[1]
+                cliente.notificar_evento(nome_arquivo)
+                self.interesses.remove(interesse)
+
+if __name__ == "__main__":
+    servidor = ThreadedServer(ServidorArquivo(), port=18812)
+    servidor.start()
